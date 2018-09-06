72bfdc7c9548b78e4ede1bc28b8853b99eab1af5
modeste Agbakou <modestemode@live.fr>
2018-09-06 14:17:21 +0000

    for add Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require('gulp');
-
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
 gulp.task('hello', function() {
     console.log('Hello Zell');
   });
 
-  gulp.task('sass', function() {
-    return gulp.src('./app/scss/styles.scss') // Gets all files ending with .scss in app/scss and children directories
+  // Compile sass into CSS 
+// gulp.task('sass', function() {
+//   return gulp.src('./app/scss/styles.scss') // Gets all files ending with .scss in app/scss and children directories
+//     .pipe(sass())
+//     .pipe(gulp.dest('./dist/css'))
+// });
+
+  // Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+      server: "./app"
+  });
+
+  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("app/scss/*.scss")
       .pipe(sass())
-      .pipe(gulp.dest('./dist/css'))
-  })
+      .pipe(gulp.dest("app/css"))
+      .pipe(browserSync.stream());
+});
+
+
+
+gulp.task('default', ['serve']);
